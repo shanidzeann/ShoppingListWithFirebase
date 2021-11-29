54491fff2a11aaac588712c4b2ca7a4c34f9a6ea
shanidzeann <shanidzeann@mail.ru>
2021-11-29 23:05:56 +0300

    Change project structure

--- ShoppingListWithFirebase/Managers/DatabaseManager.swift
+++ ShoppingListWithFirebase/Managers/DatabaseManager.swift
@@ -18,12 +18,10 @@ class DatabaseManager {
     public func insertNewUser(with email: String, user: AuthDataResult?, completion: @escaping (Bool) -> Void) {
         database.child((user?.user.uid)!).setValue(["email" : user?.user.email]) { error, _ in
             if error == nil {
-                // succeeded
                 completion(true)
                 print("insert")
                 return
             } else {
-                // failed
                 completion(false)
                 return
             }
